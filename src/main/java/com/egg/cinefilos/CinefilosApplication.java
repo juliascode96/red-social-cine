5bdb213d08ee7338268eebe7259e03eadc1d375d
Julia Scodelari <j.scodelari96@gmail.com>
2022-01-17 20:13:01 -0300

    primer commit

---
+++ src/main/java/com/egg/cinefilos/CinefilosApplication.java
@@ -0,0 +1,13 @@
+package com.egg.cinefilos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinefilosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinefilosApplication.class, args);
+	}
+
+}
