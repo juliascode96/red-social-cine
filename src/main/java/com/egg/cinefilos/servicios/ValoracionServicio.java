c3526b05c7804dc34028db27b758e1a2d10b53c6
rodrigoparis <71674495+rodrigoparis@users.noreply.github.com>
2022-02-10 17:02:01 -0300

    probando servicio
    probando servicio de valoracion con validacion d usuiario

--- src/main/java/com/egg/cinefilos/servicios/ValoracionServicio.java
+++ src/main/java/com/egg/cinefilos/servicios/ValoracionServicio.java
@@ -49,4 +49,33 @@ public class ValoracionServicio {
             throw new ErrorServicio("Pelicula no encontrada");
         }
     }
+    
+     /*@Transactional
+    public Valoracion valorar(Valoracion valoracion, Long idPelicula, Usuario usuario) throws ErrorServicio {
+    
+          if  (valoracion.getChequeoUsuarios().contains(usuario.getId())){ //me fijo si el set tiene guardado el id del usuario, 
+          //si lo tiene no dejo hacer la valoracion, si no lo tiene dejo que siga el proceso
+            throw new ErrorServicio("Usuario ya valido esta pelicula");
+          }
+    
+    
+        if(valoracion.getUltima() > 5 || valoracion.getUltima() < 1) {
+            throw new ErrorServicio("Valoración no válida");
+        }
+
+        Optional<Pelicula> respuesta = repopeli.findById(idPelicula);
+
+        if (respuesta.isPresent()) {
+            Pelicula p1 = respuesta.get();
+            Valoracion v1 = repoValoracion.findByPeliculaId(idPelicula);
+
+            v1.setCantidad(v1.getCantidad()+1);
+            v1.setTotal(v1.getTotal()+valoracion.getUltima());
+            v1.setPromedio(v1.getTotal()/v1.getCantidad());
+            valoracion.getChequeoUsuarios().add(usuario.getID()); //Guardo el usuario q hizo la valoracion en el set
+            return repoValoracion.save(v1);
+        } else {
+            throw new ErrorServicio("Pelicula no encontrada");
+        }
+    }*/
 }
